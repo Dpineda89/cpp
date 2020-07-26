9c1cd8934a2904b93a37c492ceac6d1b0bd2b4bf
Darwin Pineda <darwin_pineda@unah.hn>
2020-07-25 22:47:07 -0600

    Operador logico and

---
+++ Ejercicios/Ejercicio11-Operador-And/operador-and.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a=50;
+    int b=10;
+    int c=7;
+
+    if (a > b && a > c)
+    {
+        cout << "A es el mayor: " << a;       
+    }
+
+    if (b > a && b > c )
+    {
+        cout << "B es el mayor: " << b;
+    } 
+
+    if (c > a && c > b )
+    {
+        cout << "C es el mayor: " << c;      
+    }   
+    return 0;
+}
